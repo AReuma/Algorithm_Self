3dab4236915b05976dd7e9dbbd42869f8897f316
sinareum <kuuniin@gmail.com>
2022-09-27 18:42:06 +0900

    2차원 배열

---
+++ algorithm/src/Sept_27/arrTest1.java
@@ -0,0 +1,21 @@
+package Sept_27;
+
+public class arrTest1 {
+    public static void main(String[] args) {
+        int[][] arr1 = {{45, 50, 75}, {89}, {12, 32, 14}};
+
+        System.out.println(arr1[0].length);
+        System.out.println(arr1[1].length);
+        System.out.println(arr1[2].length);
+        System.out.println(arr1[0][0]);
+        System.out.println(arr1[0][1]);
+        System.out.println(arr1[2][1]);
+        System.out.println(arr1[1][0]);
+
+        System.out.println("==================");
+        int[] arr2 = {1, 2, 3, 4, 5};
+
+        System.out.println(arr2.length);
+        System.out.println(arr2[2]);
+    }
+}
